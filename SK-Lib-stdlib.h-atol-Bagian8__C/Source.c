de5eb55470f43839e72a93d31a46108fbf51128f
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-02-05 08:44:50 +0700

    Initial commit

---
+++ SK-Lib-stdlib.h-atol-Bagian8__C/Source.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <conio.h>
+
+/*
+    Source by Microsoft (https://docs.microsoft.com/en-us/cpp)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main(void) {
+    char* str = NULL;
+    long    value = 0;
+
+    str = "  -2309 ";
+    value = atol(str);
+    printf("Function: atol( \"%s\" ) = %d\n", str, value);
+
+    str = "314127.64";
+    value = atol(str);
+    printf("Function: atol( \"%s\" ) = %d\n", str, value);
+
+    str = "3336402735171707160320";
+    value = atol(str);
+    printf("Function: atol( \"%s\" ) = %d\n", str, value);
+
+    if (errno == ERANGE) {
+        printf("Overflow condition occurred.\n");
+    }
+
+    _getch();
+    return 0;
+}
